ced424174c754611c1a8d5ceddb7dd901f62ea0f
Yampanis <mjyampa@hotmail.com>
2025-02-19 10:33:34 -0800

    Update feedly.py

--- feedly.py
+++ feedly.py
@@ -212,6 +212,7 @@ def initialize_global_variables():
     try:
         negative_keywords_df = pd.read_excel(r'negatives.xlsx', sheet_name='Sheet1')
         negative_keywords = negative_keywords_df['Negative'].tolist()
+        logging.info(f"'{len(negative_keywords)}' negative keywords")
     except Exception as ex:
         print('Error negatives: '+ str(ex))
         negative_keywords = []
